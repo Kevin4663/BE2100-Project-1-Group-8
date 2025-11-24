89a5d46ec53847351d1911e5ef8198f017889b44
Kevin Hin <kevinhin4972@gmail.com>
2025-11-24 18:41:19 -0500

    Refactor stock data fetching: remove fetch_csv function and implement StockAnalysis class for CSV download functionality

---
+++ StockAnalysis.py
@@ -0,0 +1,44 @@
+import yfinance as yf
+from datetime import datetime
+
+class StockAnalysis:
+
+    # constructor
+    def __init__(self, company_ticker, start_date, end_date, interval, path):
+        self.company_ticker = company_ticker
+        self.start_date = start_date
+        self.end_date = end_date
+        self.interval = interval
+        self.path = path
+        self.data = None
+
+    # Downloads stock data for a given ticker and range, saves it to a csv
+    def download_csv(self):
+        # download data 
+        print(f"Downloading {self.company_ticker} data:")
+        self.data = yf.download(self.company_ticker, start=self.start_date, end=self.end_date, interval=self.interval)
+
+        # Validation
+        if not self.data.empty:
+            # Save to csv
+            self.data.to_csv(self.path)
+            print(f"Success! Data saved to {self.path}")
+        else:
+            print("An Error Occured, no data retrived")
+
+    # Method to plot data via a histogram and boxplot Part A
+
+    # Method to perform statistical analysis, statistical intervals and hypothesis test Part B and C
+
+    # Method for ML algo Part D
+
+
+# Test Example
+if __name__ == "__main__":
+    # Example: Fetch NVIDIA data from Jan 1, 2024 to today
+    sa1 = StockAnalysis(company_ticker="NVDA",
+        start_date="2024-01-01",
+        end_date=datetime.today().strftime('%Y-%m-%d'),
+        interval="1d",
+        path="nvda_stock_data.csv")
+    sa1.download_csv()
--- main.py
+++ main.py
@@ -1,21 +1,11 @@
-from fetch_csv import fetch_csv
+from StockAnalysis import StockAnalysis
 
 def main():
-    ticker = "AAPL"
-
-    fetch_csv(ticker)
-    
+    sa1 = StockAnalysis("NVDA", "2024-01-01", "2025-01-01", "1d", "temp.csv")
+    sa1.download_csv()
 
 if __name__ == "__main__":
     main()
 
-import yfinance as yf
-import pandas as pd
-from datetime import datetime
-
-# fetch a csv for a ticker from start to end in set intervals saved to a given pth
-def fetch_csv(company_ticker):
-    ticker = yf.Ticker(company_ticker)
-    print(ticker.financials)
 
 
