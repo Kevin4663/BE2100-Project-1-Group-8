8a8c35bedbc33e00b8378679e8f2d206d6fbe5e7
Kevin Hin <kevinhin4972@gmail.com>
2025-11-26 18:47:15 -0500

    Refactor main function: adjust indentation for improved readability and ensure main function is called correctly

--- main.py
+++ main.py
@@ -4,18 +4,13 @@ from datetime import datetime
 def main():
     tickers=["NVDA", "AMD", "MSFT", "AAPL"]
     sa1 = StockAnalysis(tickers, 
-                        start_date="2023-01-01", 
-                        end_date=datetime.today().strftime('%Y-%m-%d'),
-                        interval="1d", 
-                        path="stock_data.csv")
+        start_date="2023-01-01", 
+        end_date=datetime.today().strftime('%Y-%m-%d'),
+        interval="1d", 
+        path="stock_data.csv")
     sa1.fetch_data()
     for i in range(len(tickers)):
         sa1.plot_dashboard(i)
 
-
-
 if __name__ == "__main__":
     main()
-
-
-
