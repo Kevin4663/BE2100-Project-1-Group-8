e2641529d192b8b1b571278bba0c6b1c62fea928
Kevin Hin <kevinhin4972@gmail.com>
2025-11-25 23:45:55 -0500

    Enhance StockAnalysis class: improve histogram and boxplot methods, add statistical analysis functionality, and update main function for testing

--- StockAnalysis.py
+++ StockAnalysis.py
@@ -1,8 +1,8 @@
 import yfinance as yf
 from matplotlib import pyplot as plt
-import pandas as pd
-import seaborn as sns
 from datetime import datetime
+from scipy import stats
+
 
 class StockAnalysis:
     # Constructor
@@ -54,43 +54,88 @@ class StockAnalysis:
     # Method to plot data via a histogram and boxplot Part A
     # ticker_index (int) 
     # column (str)
+    # percent_change (bool)
+
     def plot_histogram(self, ticker_index, column, percent_change = False):
         if self.data is None:
             print("No data to plot. Run fetch_data() first.")
             return
-        
-        if not percent_change:
-            plt.hist(self.data[self.tickers[ticker_index]][column], edgecolor='black')
-            plt.xlabel(f"{column} Prices")
-            plt.ylabel("Frequency")
-            plt.title(f"{column} Price Frequency {self.start_date} to {self.end_date} n={self.data.shape[0]}")
-            plt.show()
-        else:
-            plt.hist(self.data[self.tickers[ticker_index]][column].pct_change(), edgecolor='black')
-            plt.xlabel(f"Percent Change in Close Price")
-            plt.ylabel("Frequency")
-            plt.title(f"Percent change in {column} Price Frequency {self.start_date} to {self.end_date} n={self.data.shape[0]}")
-            plt.show()
+        plt.figure(figsize=(10,6))
 
-    def plot_botplot(self):
+        if not percent_change:
+            plt.hist(self.data[self.tickers[ticker_index]][column].dropna(), edgecolor='black')
+            plt.xlabel(f"{column} Prices")
+            plt.title(f"{column} Price | {self.start_date} to {self.end_date} n={self.data.shape[0]}")
+        else:
+            plt.hist(self.data[self.tickers[ticker_index]][column].pct_change().dropna(), edgecolor='black')
+            plt.xlabel(f"Daily Increase")
+            plt.title(f"Daily Increase in {column} Price | {self.start_date} to {self.end_date} n={self.data.shape[0]}")
+
+        plt.ylabel("Frequency")
+        plt.show()
+
+    def plot_botplot(self, ticker_index, column):
         if self.data is None:
             print("No data to plot. Run fetch_data() first.")
             return
 
+        plt.figure(figsize=(10, 6))
+
+        marks = dict(markerfacecolor='red', marker='o')
+        mean = dict(markerfacecolor='green', marker='D', markeredgecolor='green')
+        plt.boxplot(self.data[self.tickers[ticker_index]][column].dropna(), vert=False, flierprops=marks, showmeans=True, meanprops=mean, notch=True)
+        
+        plt.title(f"Outlier Detection: {self.tickers[ticker_index]} {column} Sold per day")
+        plt.xlabel(f"{column}")
+        plt.show()
+
     # Method to perform statistical analysis, statistical intervals and hypothesis test Part B and C
+    def perform_statistical_analysis(self, ticker_index):
+        if self.data is None:
+            print("No data to plot. Run fetch_data() first.")
+            return
 
+        series = self.data[self.tickers[ticker_index]]['Close'].dropna()
+
+        # (95% Confidence Interval)
+        mean_val = series.mean()
+        sem = stats.sem(series) # Standard Error of mean
+        # Calculate Interval
+        ci = stats.t.interval(0.95, len(series)-1, loc=mean_val, scale=sem)
+
+        print(f"Statistical Analysis for {self.tickers[ticker_index]}")
+        print(f"Mean Close Price: ${mean_val:.2f}")
+        print(f"95% Confidence Interval: ${ci[0]:.2f} to ${ci[1]:.2f}")
+
+        # Hypothesis Test 
+        # Hypothesis: Is the daily return significantly different from 0
+        daily_returns = series.pct_change().dropna()
+        t_stat, p_val = stats.ttest_1samp(daily_returns, 0)
+
+        print("Hypothesis: Is the daily return significantly different from 0")
+        print(f"Hypothesis Test (T-Test) P-Value: {p_val:.5f}")
+        if p_val < 0.05:
+            print("Conclusion: Reject Null (Price movement is not random)")
+        else:
+            print("Conclusion: Fail to Reject Null (Price movement is random)")
+    
     # Method for ML algo Part D
-
+    def perform_ml_analysis(self, ticker_index):
+        if self.data is None:
+            print("No data to plot. Run fetch_data() first.")
+            return
 
 # Test Example
 if __name__ == "__main__":
     # Example: Fetch a set of stock data from Jan 1, 2024 to today
-    sa1 = StockAnalysis(tickers=["NVDA", "AMD", "INTC"],
-        start_date="2020-01-01",
+    sa1 = StockAnalysis(tickers=["AMD"],
+        start_date="2025-01-01",
         end_date=datetime.today().strftime('%Y-%m-%d'),
         interval="1d",
         path="stock_data.csv"
     )
 
     sa1.fetch_data()
-    sa1.plot_histogram(0, 'Close', True)
+    #sa1.plot_histogram(0, 'Close', True)
+    #sa1.plot_botplot(0, 'Volume')
+    sa1.perform_statistical_analysis(0)
